d5fe9b0fcccf165015fc25e45d070555b03142fb
Ajitsen Surendran <ajitsen_s@yahoo.co.in>
2018-04-25 13:13:33 +0530

    Adding git ignore, removing progressive service, public index.html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// Not using service worker
+// import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// We will not be using service worker - used in progressive webapp
+// https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#making-a-progressive-web-app
+// registerServiceWorker();
